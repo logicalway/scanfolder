c385ade466365446883f036a84ca81ffb4ace5ea
Solutions Center <support@solutionscenter.ch>
2013-12-19 23:25:11 -0800

    Add scanfolder

---
+++ scanfolder.php
@@ -0,0 +1,110 @@
+<?php
+/*
+*
+*	Scan Dossier
+*
+*	Copyright (c) 2013 Solutions Center
+*	Date creation : 01/04/13
+*	Derniere modification : 01/04/13
+*
+*	http://www.bala-krishna.com/php-recursively-file-folder-scan
+*
+*/
+
+
+// dossier de depart
+$dir = $_SERVER["DOCUMENT_ROOT"]."/".dirname($_SERVER["SCRIPT_NAME"]);
+$dir = str_replace("//","/",$dir);
+
+
+// init tab
+$tabf = array();
+$level1 = count( explode("/",$dir) );
+$niveau = 4;
+$niveau += $level1;
+
+
+// exclude
+$exeption = array(
+				"",
+				".",
+				"..",
+				"_DEL",
+				"stats",
+				"cache",
+				"session",
+				"tmp",
+				"product",
+				"code",
+);
+
+
+function list_recursive($dir,$niveau,$dirstart,$exeption,$tabf) { 
+	$level2 = count( explode("/",$dir) );
+	if( $dh = opendir($dir) ) {
+		if( $level2 < $niveau ) {
+			while(false !== ($entry = readdir($dh))) {
+				if( array_search($entry,$exeption) ) {
+					continue;
+				}
+				$path = str_replace("//","/",$dir."/".$entry);
+				$tabf[] = filemtime($path).",".str_replace($dirstart,"",$path);
+				if( is_dir($path) ) {
+					$tabf = list_recursive($path,$niveau,$dirstart,$exeption,$tabf);
+				}
+			}
+		}
+		closedir($dh);
+	}
+	return $tabf;
+}
+
+
+
+
+?>
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+<title>scan folder</title>
+
+<style type="text/css">
+
+</style>
+
+</head>
+<body>
+
+<?php
+
+
+$tab = list_recursive($dir,$niveau,$dir,$exeption,$tabf);
+
+/*
+echo '<pre>';
+print_r($tab);
+echo '</pre>';
+exit();
+*/
+
+
+rsort($tab);
+
+
+foreach( $tab as $keyF => $valueF ) {
+
+	if( preg_match("#,#",$valueF) ) {
+		$mtab = explode(",",$valueF);
+		$time = $mtab[0];
+		$path = $mtab[1];
+		echo date("Y-m-d H:m:s",$time)." - ".$path."<br />\n";
+	}
+
+}
+
+
+?>
+
+</body>
+</html>
